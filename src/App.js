e68ba9c3584296fdc53502cd33c929e1d699de7e
No0b-master <ahmadaug20@gmail.com>
2023-07-24 17:13:09 +0530

    frist0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React, { useState, useEffect } from 'react';
+import generateEmoji from './emoji';
+import './App.css';
 function App() {
+  
+  const [gaali, setGaali] = useState('');
+  const [emoji , setEmoji] = useState('😝')
+
+  useEffect(() => {
+    generateGaali()
+  }, []);
+
+  const generateGaali = () => {
+    fetch('http://159.65.159.8/profile_approval/hindi_abuse', { mode: 'no-cors' })
+      .then(function (response) {
+        console.log(response);
+        setEmoji(generateEmoji)
+
+        
+        setGaali(response.abuse)
+
+
+      }).catch(function (error) {
+        console.log('Request failed', error)
+      });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p style={{fontSize:128}}>{emoji}</p>
+
+        <h3>{gaali}</h3>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Random Gaali Generator
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button style={{
+          marginTop:'32px',
+          padding: '16px',
+          width: '120px',
+          boxShadow: '0px 0px 7px 2px gray',
+          background: 'antiquewhite',
+          borderRadius: '8px'
+        }} onClick={generateGaali}>generate</button>
       </header>
+
+
+
     </div>
   );
 }
